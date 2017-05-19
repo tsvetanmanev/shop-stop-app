2060036bbfd13b6dc4055fba9054a59e1899c40b
tsvetanmanev <tsvetanmanev@gmail.com>
2017-05-19 14:34:21 +0300

    Added folder structure and working homepage

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log(1 + 3)
+const http = require('http')
+const port = 3000
+const handlers = require('./handlers')
+
+http.createServer((req, res) => {
+  for (let handler of handlers) {
+    if (!handler(req, res)) {
+      break
+    }
+  }
+}).listen(port)
